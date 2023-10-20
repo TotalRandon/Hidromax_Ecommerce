46ce9a6ccac676017ac60c0e17dc8ee574742acf
Joao Victor <usuario@MacBook-Pro-de-usuario.local>
2023-10-20 14:35:32 -0300

    Categoria de produtos

---
+++ app/Http/Controllers/admin/CategoryController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CategoryController extends Controller
+{
+    public function index(Request $request)
+    {
+        $categories = Category::latest();
+
+        if(!empty($request->get('keyword'))) {
+            $categories = $categories->where('name', 'like', '%'.$request->get('keyword').'%');
+        }
+
+        $categories = $categories->paginate(10);
+        
+        return view('admin.category.list', compact('categories'));
+    }
+
+    public function create()
+    {
+        return view('admin.category.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'slug' => 'required|unique:categories'
+        ]);
+
+        if($validator->passes()) {
+        
+            $category = new Category();
+            $category->name = $request->name;
+            $category->slug = $request->slug;
+            $category->status = $request->status;
+            $category->save();
+
+            session()->flash('success', 'Categoria adicionada com sucesso!');
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Categoria adicionada com sucesso!'
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => 'Falha na validação'
+            ]);
+        }
+    }
+
+    public function edit()
+    {
+        # code...
+    }
+
+    public function destroy()
+    {
+        # code...
+    }
+}
--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="#" class="brand-link" style="text-align: center">
+    <a href="{{ route('admin.dashboard') }}" class="brand-link" style="text-align: center">
         <!-- <img src="{{'admin-asset/img/AdminLTELogo.png'}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
         <span class="brand-text font-weight-hight">HIDROMAX</span>
     </a>
@@ -12,13 +12,13 @@
                 <!-- Add icons to the links using the .nav-icon class
                     with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="dashboard.html" class="nav-link">
+                    <a href="{{ route('admin.dashboard') }}" class="nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>Dashboard</p>
                     </a>																
                 </li>
                 <li class="nav-item">
-                    <a href="categories.html" class="nav-link">
+                    <a href="{{ route('categories.index') }}" class="nav-link">
                         <i class="nav-icon fas fa-file-alt"></i>
                         <p>Category</p>
                     </a>
