9d7e7c7ccc838420704c9209b795d7d1efb0de1f
Joao Victor <usuario@MacBook-Pro-de-usuario.local>
2023-10-24 15:44:27 -0300

    Categoria atualizar, deletar e adicionar imagens

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
+use App\Models\TempImage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Validator;
+use Image;
 
 class CategoryController extends Controller
 {
@@ -42,6 +45,30 @@ class CategoryController extends Controller
             $category->status = $request->status;
             $category->save();
 
+            // Save image here!
+            if(!empty($request->image_id)) {
+                $tempImage = TempImage::find($request->image_id);
+                $extArray = explode('.', $tempImage->name);
+                $ext = last($extArray);
+
+                $newImageName = $category->id.'.'.$ext;
+                $sPath = public_path().'/temp/'.$tempImage->name;
+                $dPath = public_path().'/uploads/category/'.$newImageName;
+                File::copy($sPath, $dPath);
+
+                // Generate Image Thumbnail
+                $dPath = public_path().'/uploads/category/thumb/'.$newImageName;
+                $img = Image::make($sPath);
+                //$img->resize(450, 600);
+                $img->fit(450, 600, function($constraint) {
+                    $constraint->upsize();
+                });
+                $img->save($dPath);
+
+                $category->image = $newImageName;
+                $category->save();
+            }
+
             session()->flash('success', 'Categoria adicionada com sucesso!');
 
             return response()->json([
@@ -51,18 +78,117 @@ class CategoryController extends Controller
         } else {
             return response()->json([
                 'status' => false,
-                'message' => 'Falha na validação'
+                'message' => $validator->errors()
             ]);
         }
     }
 
-    public function edit()
+    public function edit($categoryId, Request $request)
     {
-        # code...
+        $category = Category::find($categoryId);
+        if(empty($category)) {
+            return redirect()->route('categories.index');
+        }
+
+        return view('admin.category.edit',compact('category'));
     }
 
-    public function destroy()
+    public function update($categoryId, Request $request)
     {
-        # code...
+        $category = Category::find($categoryId);
+
+        if(empty($category)) {
+
+            session()->flash('error', 'Categoria não encontrada');
+
+            return response()->json([
+                'status' => false,
+                'notFound' => true,
+                'message' => 'Categoria não encontrada'
+            ]);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'slug' => 'required|unique:categories,slug,'.$category->id.',id'
+        ]);
+
+        if($validator->passes()) {
+        
+            $category->name = $request->name;
+            $category->slug = $request->slug;
+            $category->status = $request->status;
+            $category->save();
+
+            $oldImage = $category->image;
+
+            // Save image here!
+            if(!empty($request->image_id)) {
+                $tempImage = TempImage::find($request->image_id);
+                $extArray = explode('.', $tempImage->name);
+                $ext = last($extArray);
+
+                $newImageName = $category->id.'-'.time().'.'.$ext;
+                $sPath = public_path().'/temp/'.$tempImage->name;
+                $dPath = public_path().'/uploads/category/'.$newImageName;
+                File::copy($sPath, $dPath);
+
+                // Generate Image Thumbnail
+                $dPath = public_path().'/uploads/category/thumb/'.$newImageName;
+                $img = Image::make($sPath);
+                //$img->resize(450, 600);
+                $img->fit(450, 600, function($constraint) {
+                    $constraint->upsize();
+                });
+                $img->save($dPath);
+
+                $category->image = $newImageName;
+                $category->save();
+
+                // Delete old images
+                File::delete(public_path().'/uploads/category/thumb/'.$oldImage);
+                File::delete(public_path().'/uploads/category/'.$oldImage);
+            }
+
+            session()->flash('success', 'Categoria atualizada com sucesso!');
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Categoria atualizada com sucesso!'
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => $validator->errors()
+            ]);
+        }
+    }
+
+    public function destroy($categoryId, Request $request)
+    {
+        $category = Category::find($categoryId);
+
+        if(empty($category)) {
+            
+            session()->flash('error', 'Categoria não encontrada.');
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Categoria não encontrada.'
+            ]);
+            //return redirect()->route('categories.index');
+        }
+
+        File::delete(public_path().'/uploads/category/thumb/'.$category->image);
+        File::delete(public_path().'/uploads/category/'.$category->image);
+
+        $category->delete();
+
+        session()->flash('success', 'Categoria deletada com sucesso!');
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Categoria deletada com sucesso!'
+        ]);
     }
 }
